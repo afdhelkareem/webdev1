f9f748195464cbea3725e1826c9b3bb6b02bf363
afdhelkareem <162284319+afdhelkareem@users.noreply.github.com>
2024-05-23 22:22:31 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header';
+import GoalList from './component/GoalList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hello, this is afdhel!</h1>
-        <p>i'm 19 years old and live in jakarta</p>
-        <p>My full name is Muhamad Afdhel Kareem Jabbar and I am now studying in Padjadjaran University.</p>
-      </header>
+      <Header />
+      <GoalList />
     </div>
   );
 }
